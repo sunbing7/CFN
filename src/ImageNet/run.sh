05bcf0bdfbcc12fb5b7f5af504144239a16b97f0
sunbing7 <bing.sun.2020@phdcs.smu.edu.sg>
2024-04-03 16:27:21 +0800

    add test script

--- src/ImageNet/run.sh
+++ src/ImageNet/run.sh
@@ -6,7 +6,7 @@
 
 for TARGET_CLASS in {755,743,804,700,922,174,547,369}
 do
-    python patchattack_models_danse.py --clp=0--model_type=0 --data=/root/autodl-tmp/sunbing/workspace/uap/data/imagenet/validation/ --uap_path=/root/autodl-tmp/sunbing/workspace/uap/CFN/src/ImageNet/uap/ --target=$TARGET_CLASS
-    python patchattack_models_danse.py --clp=1.0--model_type=0 --data=/root/autodl-tmp/sunbing/workspace/uap/data/imagenet/validation/ --uap_path=/root/autodl-tmp/sunbing/workspace/uap/CFN/src/ImageNet/uap/ --target=$TARGET_CLASS
-    python patchattack_models_danse.py --clp=1.1--model_type=0 --data=/root/autodl-tmp/sunbing/workspace/uap/data/imagenet/validation/ --uap_path=/root/autodl-tmp/sunbing/workspace/uap/CFN/src/ImageNet/uap/ --target=$TARGET_CLASS
+    python patchattack_models_danse.py --clp=0.0 --model_type=0 --data=/root/autodl-tmp/sunbing/workspace/uap/data/imagenet/validation/ --uap_path=/root/autodl-tmp/sunbing/workspace/uap/CFN/src/ImageNet/uap/ --target=$TARGET_CLASS
+    python patchattack_models_danse.py --clp=1.0 --model_type=0 --data=/root/autodl-tmp/sunbing/workspace/uap/data/imagenet/validation/ --uap_path=/root/autodl-tmp/sunbing/workspace/uap/CFN/src/ImageNet/uap/ --target=$TARGET_CLASS
+    python patchattack_models_danse.py --clp=1.1 --model_type=0 --data=/root/autodl-tmp/sunbing/workspace/uap/data/imagenet/validation/ --uap_path=/root/autodl-tmp/sunbing/workspace/uap/CFN/src/ImageNet/uap/ --target=$TARGET_CLASS
 done
